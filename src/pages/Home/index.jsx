31785a1fcc82ef51933b041561d22ead8f6a2bad
fabio bueno <fabiobueno@outlook.com>
2024-08-21 22:11:46 -0300

    Criando router

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home () {
+
+  document.title = 'Alphamil Brindes - Início';
+
+  return (
+      <>
+        <h1>Home</h1>
+      </>
+    );
+};
+
+export default Home;
